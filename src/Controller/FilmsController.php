0110acca5c022b44e94a7a8d06a2e030086b246e
Robin <gressier.robin@gmail.com>
2023-04-24 17:39:34 +0200

    template details film

--- src/Controller/FilmsController.php
+++ src/Controller/FilmsController.php
@@ -2,18 +2,21 @@
 
 namespace App\Controller;
 
+use App\Entity\Films;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class HomeController extends AbstractController
+#[Route('/films', name: 'films_')]
+class FilmsController extends AbstractController
 {
-    #[Route('/', name: 'home')]
-    public function index(): Response
+    #[Route('/{slug}', name: 'details_')]
+    public function details(Films $film): Response
     {
-        return $this->render('home.html.twig', [
-            'controller_name' => 'HomeController',
-        ]);
+        return $this->render(
+            'films/details.html.twig',
+            compact('film')
+        );
     }
 }
 // class HomeController extends AbstractController
